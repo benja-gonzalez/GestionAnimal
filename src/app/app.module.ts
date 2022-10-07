15edc4ba9c614f1f132747de8839ac77f1f87a87
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2022-10-07 15:47:39 -0300

    Ajustes de module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// FONTAWESOME
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
